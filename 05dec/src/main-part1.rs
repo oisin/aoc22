4fe07ebf79acb5ee10f7ca543454197c1389ef2d
Oisin Hurley <oisin.hurley@gmail.com>
2022-12-05 22:45:13 +0000

    save point for Puzzle 5

--- 05dec/src/main-part1.rs
+++ 05dec/src/main-part1.rs
@@ -19,6 +19,70 @@ fn main() {
             }
         }
         println!(" stack looks like {:?}", stack_data);
+
+        // The number at the base of the stack pinpoints the column where the 
+        // crate identifier is. Follow the columns to create vectors for each
+        // stack of crates.
+
+        let indices = stack_data.last().unwrap();
+        let boop: Vec<usize> = indices
+            .chars()
+            .enumerate()
+            .map(|(i, c)| {
+                if !c.is_whitespace() {
+                   return i;
+                }
+                return 0;
+            })
+            .filter(|&i| i != 0)
+            .collect();
+
+        //println!("indexes are {:?}", boop);
+
+        // boop is a vector containing the columns we need to check to pull
+        // out the crate labels from the right place in the loaded strings 
+        // to create the vectors.
+        //
+        // In the test data, the columns are [1,5,9]. Three stacks then, in
+        // a vector themselves, in order. 
+
+        println!("{} columns: {:?}", boop.len(), boop);
+
+        let mut stacks: Vec<Vec<char>> = Vec::new();
+
+        for _ in 1..=boop.len() {
+            stacks.push(Vec::new());
+        }
+
+        println!("stacks vec: {:?}", stacks);
+
+        let mut extract_keys = true;
+        for stackline in stack_data.iter().rev() {
+            if extract_keys {
+                extract_keys = false;
+                continue;
+            }
+            for (inx, pos) in boop.iter().enumerate() {
+                let target_char = stackline.chars().nth(*pos).unwrap();
+                if !target_char.is_whitespace() {
+                    stacks[inx].push(target_char);
+                }
+            }
+        }
+
+        // from the test data, the above will make:  [['Z', 'N'], ['M', 'C', 'D'], ['P']]
+        // now the moves can begin - read the moves from the input file
+        //    move 1 from 2 to 1
+        // split on space -> [move, 1, from, 2, to, 1]
+        // relevant instructions are on inx [1,3,5]
+        // convert to usize
+        // : number to pop 
+        // : index of the source vector + 1
+        // : index of the target vector + 1
+
+        
+
+        println!("hash loks like: {:?}", stacks)
     }
 }
 
